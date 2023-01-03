612f41249765d5ff2c03f1f9b6c161d9b3e64215
Vasilievkosta <vasilievkosta@gmail.com>
2023-01-03 23:19:30 +0200

    added homework 08

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,31 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8'
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            let sortState = [...state];
+            sortState.sort(function (a, b) {
+                if (a.name > b.name) {
+                    return 1;
+                }
+                if (a.name < b.name) {
+                    return -1;
+                }
+                // a должно быть равным b
+                return 0;
+            });
+            if (action.payload === 'down') {
+                return sortState.reverse()
+            }
+            return sortState // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            let changeAge = state.filter(t => t.age > action.payload)
+            return changeAge // need to fix
         }
         default:
             return state
